39efa4fc36ad444f23fca65fbf8c541168cfff19
LAPTOP-OF-TZM\IamTZM <3128703967@qq.com>
2020-04-04 08:34:03 +0800

    add 53

---
+++ src/DynamicProgramming/Solution53.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+/**
+ * 53. Maximum Subarray
+ */
+public class Solution53 {
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        int tmp = nums[0], res = nums[0];
+        for (int i = 1; i < len; i++) {
+            tmp = Math.max(tmp + nums[i], nums[i]);
+            res = Math.max(res, tmp);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] tst = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(tst));
+    }
+}
