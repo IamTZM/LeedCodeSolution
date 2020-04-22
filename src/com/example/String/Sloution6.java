348daf823c4b4dcc2dba5f90af79b19d7d833300
LAPTOP-OF-TZM\IamTZM <3128703967@qq.com>
2020-04-22 12:43:46 +0800

    add 6

---
+++ src/com/example/String/Sloution6.java
@@ -0,0 +1,40 @@
+package com.example.String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sloution6 {
+
+    public static String convert(String s, int numRows) {
+        if (s == null || s.length() <= 2 || numRows == 1) {
+            return s;
+        }
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuilder[] lines = new StringBuilder[numRows];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = new StringBuilder();
+        }
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) {
+                lines[idx].append(c[i++]);
+            }
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--) {
+                lines[idx].append(c[i++]);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder line : lines) {
+            res.append(line);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 1));
+
+    }
+
+
+}
