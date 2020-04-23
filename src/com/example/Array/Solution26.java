249b99d529aead00f50c8f3d6499aab4d50fb61c
LAPTOP-OF-TZM\IamTZM <3128703967@qq.com>
2020-04-23 13:03:58 +0800

    add 416

--- src/com/example/Array/Solution26.java
+++ src/com/example/Array/Solution26.java
@@ -16,13 +16,13 @@ public class Solution26 {
             }
             if (p2 < len) {
                 p1++;
-                nums[p1]=nums[p2];
+                nums[p1] = nums[p2];
             }
         }
         return p1 + 1;
     }
 
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
     }
 }
---
+++ src/com/example/DynamicProgramming/Solution416.java
@@ -0,0 +1,35 @@
+package com.example.DynamicProgramming;
+
+public class Solution416 {
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        int target = sum / 2, len = nums.length;
+        boolean[][] dp = new boolean[len][target + 1];
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+        for (int i = 0; i < len; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (nums[i] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[len - 1][target];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
